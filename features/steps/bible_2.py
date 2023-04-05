98822f37d7a152d408738443ddf1ae92be74173f
ksharma12 <keshavsharma.tu@gmail.com>
2023-04-05 18:59:45 +0530

    New files added

--- features/steps/bible_2.py
+++ features/steps/bible_2.py
@@ -13,11 +13,11 @@ use_step_matcher("parse")
 def step_impl(context):
     verse = 1
     chapter = 1
-    while chapter <= 13:
+    while chapter <= 42:
         context.ele_ops = Element_Operations(context.driver)
         verse_str = str(verse)
         chapter_str = str(chapter)
-        context.ele_ops.get_url("https://www.biblehub.com/2_corinthians/"+chapter_str+"-"+verse_str+".htm")
+        context.ele_ops.get_url("https://www.biblehub.com/job/"+chapter_str+"-"+verse_str+".htm")
         context.ele_ops.maximize_window()
         # context.ele_ops.select_by_visible_text(oR.bible_Menu_1_dropdown__XPATH, "Gen")
         if context.ele_ops.get_element_text(oR.top_heading__XPATH) != "Bible Hub":
@@ -27,12 +27,12 @@ def step_impl(context):
             # context.ele_ops.switch_to_default_content()
             verse_text = context.ele_ops.get_text_from_multiple_elements(oR.bible_text_verse__XPATH)
             # verse_text.insert(0, chapter_str+":"+verse_str)
-            mydoc = docx.Document("E://auto_frameworks//Behave_python//features//steps//2_corinthians.docx")
+            mydoc = docx.Document("E://auto_frameworks//Behave_python//features//steps//job.docx")
             p = mydoc.add_paragraph()
             super_text = p.add_run(chapter_str + ":" + verse_str)
             super_text.font.superscript = True
             p.add_run([" " + ele + " " for ele in verse_text])
-            mydoc.save("E://auto_frameworks//Behave_python//features//steps//2_corinthians.docx")
+            mydoc.save("E://auto_frameworks//Behave_python//features//steps//job.docx")
         elif context.ele_ops.get_element_text(oR.top_heading__XPATH) == "Bible Hub":
             verse = 1
             verse -= 1
